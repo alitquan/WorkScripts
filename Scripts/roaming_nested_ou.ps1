bfe244d513c45acf6839381b391898d16f73f602
alitquan <amallick@stuy.edu>
2023-05-11 11:14:44 -0400

    uploading

---
+++ Scripts/roaming_nested_ou.ps1
@@ -0,0 +1,47 @@
+﻿$ous = Get-ADOrganizationalUnit -Filter 'Name -like "*"' | select Name, DistinguishedName 
+echo "testing" 
+foreach($ou in $ous) {
+
+   $arg1 = "$($ou.Name)"
+   $arg2 = "$($ou.DistinguishedName)" 
+
+   #some OUs will not feature roaming profiles
+   if ($arg1 -eq "redacted") {
+        continue 
+   } 
+   if ($arg1 -eq "Domain Controllers") {
+        continue
+   }
+   if ($arg1 -eq "redacted") {
+        continue
+   }
+
+
+   echo $arg1
+   $users = Get-ADUser -Filter * -SearchBase $arg2  -Properties ProfilePath, HomeDirectory, HomeDrive    
+
+
+
+   $counter = 0
+
+   # path to profile storage
+   $origProfilePath = "\\insert_path\Roaming_Profiles$\"
+
+   # path to home folder storage
+   $origLocalPath   = "\\insert_path\Roaming_Home$\" 
+   
+   foreach ($user in $users) {
+        $userAttrs = $user | Select Name, SamAccountName, ProfilePath, HomeDirectory, HomeDrive
+        echo $userAttrs
+        echo $counter
+        ++$counter
+
+        $profilePath = $origProfilePath + $($userAttrs.SamAccountName)
+        $localPath   = $origLocalPath + $($userAttrs.SamAccountName)
+        
+        Set-ADUser $($userAttrs.SamAccountName) -ProfilePath $profilePath -HomeDirectory $localPath -HomeDrive H
+   } 
+
+   
+
+}
