18f3dbd2a6aa1e735c853d0cba65225a3c03cb13
alitquan <amallick@stuy.edu>
2024-11-19 15:23:00 -0500

    local device security script
    powershell

---
+++ Scripts/local-device-security.ps1
@@ -0,0 +1,13 @@
+﻿# password policies 
+net accounts /maxpwage:90
+net accounts /minpwlen:10 
+net accounts /uniquepw:5 
+net accounts /minpwage:0 
+
+# creating storage partition 
+Resize-Partition -DriveLetter C -Size (Get-Partition -DriveLetter C).Size - 5GB
+New-Partition -DiskNumber 0 -Size 5GB -AssignDriveLetter -DriveLetter E
+Format-Volume -DriveLetter E -FileSystem NTFS -NewFileSystemLabel "Storage"
+
+# disable USB 
+Set-ItemProperty -Path "HKLM:\SYSTEM\ControlSet001\Services\USBSTOR" -Name "Start" -Value 4
