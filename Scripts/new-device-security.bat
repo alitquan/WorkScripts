900564122e0155c39f29902423129414f9651b74
alitquan <amallick@stuy.edu>
2024-11-19 15:26:24 -0500

    Delete Scripts/new-device-security.bat

--- Scripts/new-device-security.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-ECHO Setting Security Policies...
-net accounts
-net accounts /maxpwage:90
-net accounts /minpwlen:10
-net accounts /uniquepw:5
-net accounts
-reg add HKLM\SYSTEM\CurrentControlSet\Services\UsbStor /v "Start" /t REG_DWORD /d "4" /f 
-PAUSE
