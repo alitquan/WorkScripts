be64f105f7983c2cc87889616d6913d7e554cb04
alitquan <alitmallick@gmail.com>
2026-06-23 14:57:42 -0400

    Update and rename get-sysapplogs.ps1 to get-sysapp-logs.ps1

--- Scripts/get-sysapp-logs.ps1
+++ Scripts/get-sysapp-logs.ps1
@@ -8,5 +8,6 @@ Get-WinEvent -FilterHashtable @{
     LogName   = @('System','Application')
     StartTime = $StartTime
     Level     = 1,2,3   # 1=Critical, 2=Error, 3=Warning
-} | Select-Object TimeCreated, LogName, ProviderName, Id, LevelDisplayName, Message |
-    Sort-Object TimeCreated -Descending
+} | 
+Select-Object TimeCreated, LogName, ProviderName, Id, LevelDisplayName, Message |
+Sort-Object TimeCreated -Descending
